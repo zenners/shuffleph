c8a510fc1a403c487975d707adcb0e8454a6bd78
dackalacbayo <dackalacbayo@gmail.com>
2018-06-08 17:59:55 +0800

    adding of date, start time and end time fields in collections

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,13 +6,19 @@ class Footer extends Component {
     return (
       <section className="footer">
         <footer className="pl-3 pr-5">
+        <span className="icon social float-left pr-3"><a href="https://www.instagram.com/shufflemakerspace/">
+        <i className="fab fa-instagram mr-2" data-reactid="16"></i></a>
+      <a href="https://www.facebook.com/shufflemakerspace/">
+        <i className="fab fa-facebook" data-reactid="19"></i></a></span>
+
           <h4
             className="footer-mrgn"
             style={{
               color: "#000000"
             }}
           >
-            187 N. Averilla Street San Juan del Monte 1500 /<a
+
+             187 N. Averilla Street San Juan del Monte 1500 /<a
               href="/"
               style={{
                 color: "#000000",
@@ -22,6 +28,7 @@ class Footer extends Component {
               hello@shuffle.ph{" "}
             </a>
             / 0995 013 4763 / 02 404 2101
+
           </h4>
         </footer>
       </section>
