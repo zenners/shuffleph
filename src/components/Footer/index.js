dfd144c51a8a46d322c2d416f5d5e14052f947b2
dackalacbayo <33882737+dackalacbayo@users.noreply.github.com>
2018-06-14 16:30:08 +0800

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -12,13 +12,13 @@ class Footer extends Component {
               <div className="row">
                 <div className="col-lg-12 col-md-12 col-sm-6">
 
-                  <span className="" >
-                    <a className="text-left footer-content">
+                  <span>
+                    <span className="text-left footer-content">
                     187 N. Averilla Street San Juan del Monte 1500 /<a href="/">
                       hello@shuffle.ph{" "}
                     </a>
                     / 0995 013 4763 / 02 404 2101
-                    </a>
+                    </span>
                   <span>
                     <a href="https://www.facebook.com/shufflemakerspace/">
                       <i className="icon-social fab fa-facebook" />
