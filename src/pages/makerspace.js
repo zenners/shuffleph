d0bf81fa9181addb0f706352e1d830ceb8c37071
dackalacbayo <dackalacbayo@gmail.com>
2018-06-20 10:48:15 +0800

    moving form outside the localform

--- src/pages/makerspace.js
+++ src/pages/makerspace.js
@@ -350,7 +350,7 @@ class MakerSpacePage extends Component {
             >
               </LocalForm>
                 <h3 className="title-head tx-ma no-pad"> Book our Makerspace </h3>
-                <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={this.handleSubmit(values)}>
                   <input type="hidden" name="bot-field" />
                 <InfoSection />
                 <EventSec />
