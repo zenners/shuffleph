abd600ff7e340235b3850651e7e65d9ea0a1737d
dackalacbayo <dackalacbayo@gmail.com>
2018-06-21 10:39:35 +0800

    updated

--- src/pages/makerspace.js
+++ src/pages/makerspace.js
@@ -110,7 +110,6 @@ const InfoSection = () => (
       <Col sm={10}>
         <Control.input
           model=".email"
-          name="email"
           required
           component={email}
         />
@@ -295,28 +294,22 @@ const encode = (data) => {
  }
 class MakerSpacePage extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { name: "", email: "", message: "" };
-  }
-
-  // handleChange(values) { console.log(values) }
+  handleChange(values) { console.log(values) }
   handleUpdate(form) { console.log(form) }
 
 
-      handleSubmit = e => {
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({ "form-name": "contact", ...this.state })
-        })
-          .then(() => alert("Success!"))
-          .catch(error => alert(error));
 
-        e.preventDefault();
-      };
+  handleSubmit = values => {
+     fetch("/", {
+       method: "POST",
+       headers: { "Content-Type": "application/x-www-form-urlencoded" },
+       body: encode(values,{"form-name": "contact"})
+     })
+       .then(() => alert("Success!"))
+       .catch(error => alert(error));
 
-      handleChange = e => this.setState({ [e.target.name]: e.target.value });
+     // e.preventDefault();
+   };
 
 
   handleSubmitFailed(userForm) {
@@ -327,28 +320,34 @@ class MakerSpacePage extends Component {
     console.log(userForm.email.errors);
   }
   render(){
-     const { name, email, message } = this.state;
     return(
-      <form onSubmit={this.handleSubmit}>
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message" value={message} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+      <div>
+       <Header />
+        <div className="flex-wrap row-eq-height">
+          <div className="col-md-6 content-body form-container mtop">
+            <LocalForm
+              onUpdate={(form) => this.handleUpdate(form)}
+              onChange={(values) => this.handleChange(values)}
+              onSubmit={(values) => this.handleSubmit(values)}
+            >
+                <h3 className="title-head tx-ma no-pad"> Book our Makerspace </h3>
+                  <InfoSection/>
+                  <EventSec/>
+                  <Layout/>
+                  <Extras/>
+                  <Button className="bg-ma tx-og"block>Submit</Button>
+            </LocalForm>
+
+          </div>
+          <div className="col-md-6 no-pad">
+            <div className="d-flex flex-column">
+              {menu.map((item) => (
+                <MenuItem title={item.title} styles={item.styles} path={item.path} isActive={item.isActive} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
